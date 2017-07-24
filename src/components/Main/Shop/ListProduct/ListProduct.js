0f5eb37c7dfb5be29175cdb2812b820905912e9c
Truong Pham Van <truongpvuet@gmail.com>
2017-07-24 01:00:00 -0700

    List product and go back home

--- src/components/Main/Shop/ListProduct/ListProduct.js
+++ src/components/Main/Shop/ListProduct/ListProduct.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {
   View,
   Text,
-  TouchableOpacity
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native';
+import backList from '../../../../media/appIcon/backList.png';
+import sp1 from '../../../../media/temp/sp1.jpeg';
 
 export default class ListProduct extends React.Component {
   constructor(props) {
@@ -21,20 +26,152 @@ export default class ListProduct extends React.Component {
   }
 
   render() {
+    const {
+      constainer, header, wrapper, backStyle, titleStyle,
+      productContainer, productInfo, productImage, lastRowInfo,
+      txtName, txtPrice, txtMaterial, txtColor, txtShowDetail
+    } = styles;
     return (
-      <View >
-        <Text> List product here. </Text>
-        <TouchableOpacity
-          onPress={() => this.gobackHome()}
-        >
-          <Text> GO BACK HOME </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => this.gotoDetail()}
-        >
-          <Text> GO TO DETAIL </Text>
-        </TouchableOpacity>
+      <View style={constainer}>
+        <ScrollView style={wrapper}>
+
+          <View style={header}>
+            <TouchableOpacity onPress={() => this.gobackHome()}>
+              <Image source={backList} style={backStyle} />
+            </TouchableOpacity>
+            <Text style={titleStyle}> Party Address </Text>
+            <View style={{ width: 30 }} />
+          </View>
+
+          <View style={productContainer}>
+            <Image source={sp1} style={productImage} />
+            <View style={productInfo}>
+              <Text style={txtName}> Lace Sleeve Si </Text>
+              <Text style={txtPrice}> 117$ </Text>
+              <Text style={txtMaterial}> Material silk </Text>
+              <View style={lastRowInfo}>
+                <Text style={txtColor}> Colo RoyalBlue </Text>
+                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                <TouchableOpacity>
+                  <Text style={txtShowDetail}> SHOW DETAILS </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <View style={productContainer}>
+            <Image source={sp1} style={productImage} />
+            <View style={productInfo}>
+              <Text style={txtName}> Lace Sleeve Si </Text>
+              <Text style={txtPrice}> 117$ </Text>
+              <Text style={txtMaterial}> Material silk </Text>
+              <View style={lastRowInfo}>
+                <Text style={txtColor}> Colo RoyalBlue </Text>
+                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                <TouchableOpacity>
+                  <Text style={txtShowDetail}> SHOW DETAILS </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <View style={productContainer}>
+            <Image source={sp1} style={productImage} />
+            <View style={productInfo}>
+              <Text style={txtName}> Lace Sleeve Si </Text>
+              <Text style={txtPrice}> 117$ </Text>
+              <Text style={txtMaterial}> Material silk </Text>
+              <View style={lastRowInfo}>
+                <Text style={txtColor}> Colo RoyalBlue </Text>
+                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                <TouchableOpacity>
+                  <Text style={txtShowDetail}> SHOW DETAILS </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <View style={productContainer}>
+            <Image source={sp1} style={productImage} />
+            <View style={productInfo}>
+              <Text style={txtName}> Lace Sleeve Si </Text>
+              <Text style={txtPrice}> 117$ </Text>
+              <Text style={txtMaterial}> Material silk </Text>
+              <View style={lastRowInfo}>
+                <Text style={txtColor}> Colo RoyalBlue </Text>
+                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                <TouchableOpacity>
+                  <Text style={txtShowDetail}> SHOW DETAILS </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    backgroundColor: '#DBDBDB',
+  },
+  header: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 5
+  },
+  wrapper: {
+    backgroundColor: '#FFF',
+    margin: 10,
+    paddingHorizontal: 10
+  },
+  backStyle: {
+    width: 30,
+    height: 30
+  },
+  productContainer: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    borderTopColor: '#F0F0F0',
+    borderBottomColor: '#FFF',
+    borderLeftColor: '#FFF',
+    borderRightColor: '#FFF',
+    borderWidth: 1
+  },
+  titleStyle: {
+    color: '#B10D65',
+    fontSize: 20
+  },
+  productImage: {
+    width: 90,
+    height: (90 * 452) / 361
+  },
+  productInfo: {
+    justifyContent: 'space-between',
+    marginLeft: 10,
+    flex: 1
+  },
+  lastRowInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  txtName: {
+    color: '#BCBCBC',
+    fontSize: 20,
+    fontWeight: '100'
+  },
+  txtPrice: {
+    color: '#B10D65'
+  },
+  txtMaterial: {
+  },
+  txtColor: {
+  },
+  txtShowDetail: {
+    color: '#B10D65',
+    fontSize: 11
+  }
+});
